3994e64679336b2705266fbe6226fb6295381b21
enzo256 <enzosamudio@gmail.com>
2025-04-12 22:00:49 -0300

    agregadO .ENV

--- src/environments/envirorments.ts
+++ src/environments/envirorments.ts
@@ -1,14 +1,14 @@
 /// <reference types="node" />
 export const environment = {
     production: true,
-    firebase: {
-      apiKey: process.env['FIREBASE_API_KEY'],
-      authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-      projectId: process.env['FIREBASE_PROJECT_ID'],
-      storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-      messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-      appId: process.env['FIREBASE_APP_ID'],
-      measurementId: process.env['MEASUREMENT_ID'],
+    firebase: {      
+      apiKey: (window as any)['env']['FIREBASE_API_KEY'],
+      authDomain: (window as any)['env']['FIREBASE_AUTH_DOMAIN'],
+      projectId: (window as any)['env']['FIREBASE_PROJECT_ID'],
+      storageBucket: (window as any)['env']['FIREBASE_STORAGE_BUCKET'],
+      messagingSenderId: (window as any)['env']['FIREBASE_MESSAGING_SENDER_ID'],
+      appId: (window as any)['env']['FIREBASE_APP_ID'],
+      measurementId: (window as any)['env']['MEASUREMENT_ID'],
     },
-    apiBaseUrl: process.env['API_BASE_URL'],
+    apiBaseUrl: (window as any)['env']['API_BASE_URL'],
   };
