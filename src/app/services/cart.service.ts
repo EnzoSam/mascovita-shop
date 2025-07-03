56abb8f57dd14b34bcc76e10ab572948e6e25a5e
enzosamudio <enzosamudio@gmail.com>
2025-07-03 20:14:18 -0300

    redondeo de carrito

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -78,7 +78,7 @@ export class CartService {
   getTotal(cart:Cart) {
     let r = cart.products.reduce((total, item) =>
        total + item.product.discountedPrice * item.quantity, 0);
-    return r;
+    return  Math.round(r * 100) / 100;
   }  
 
   changeQuantity(productiD: number, _quantity:number) {
